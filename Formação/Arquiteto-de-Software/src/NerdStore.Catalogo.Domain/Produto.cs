bb82fc1d89469839d9444b60379d5ff4c4336213
Gabriel Batista <gabriel.batista@xpi.com.br>
2022-05-08 18:25:56 -0300

    conclusão do módulo 04 da aula 07 da seção "Modelagem de Domínios Ricos"

--- Formação/Arquiteto-de-Software/src/NerdStore.Catalogo.Domain/Produto.cs
+++ Formação/Arquiteto-de-Software/src/NerdStore.Catalogo.Domain/Produto.cs
@@ -13,9 +13,10 @@ public sealed class Produto : Entity, IAggregateRoot
     public DateTime DataCadastro { get; private set; }
     public string Imagem { get; private set; }
     public int QuantidadeEstoque { get; private set; }
+    public Dimensoes Dimensoes { get; private set; }
     public Categoria Categoria { get; private set; }
 
-    public Produto(string nome, string descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string imagem)
+    public Produto(string nome, string descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string imagem, Dimensoes dimensoes)
     {
         CategoriaId = categoriaId;
         Nome = nome;
@@ -24,6 +25,7 @@ public sealed class Produto : Entity, IAggregateRoot
         Valor = valor;
         DataCadastro = dataCadastro;
         Imagem = imagem;
+        Dimensoes = dimensoes;
 
         Validar();
     }
