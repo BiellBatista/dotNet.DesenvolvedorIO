6fb7fc8d2917ec93d9f8da3d4e1ea874985556d3
Gabriel Batista <gabrielbatista@bne.com.br>
2022-08-28 20:07:57 -0300

    peparando ambiente para a aula 05 da seção "Fundamentos de Arquitetura de Software"

---
+++ Formação/Arquiteto-de-Software/src/Fundamentos/SOLID/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SOLID;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Escolha a operação");
+        Console.WriteLine("1 - OCP");
+        Console.WriteLine("2 - LSP");
+
+        var opcao = Console.ReadKey();
+
+        switch (opcao.KeyChar)
+        {
+            case '1':
+                CaixaEletronico.Operacoes();
+                break;
+
+            case '2':
+                CalculoArea.Calcular();
+                break;
+        }
+    }
+}
