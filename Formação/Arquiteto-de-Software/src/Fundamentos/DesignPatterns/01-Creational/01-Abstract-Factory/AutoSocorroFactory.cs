5af1dfa09ba5cb2d6068da44fbefc29e179faa37
Gabriel Batista <gabriel.texteis@gmail.com>
2023-03-05 18:45:13 -0300

    conclusão do módulo 03 da aula 08 da seção "Fundamentos de Arquitetura de Software"

---
+++ Formação/Arquiteto-de-Software/src/Fundamentos/DesignPatterns/01-Creational/01-Abstract-Factory/AutoSocorroFactory.cs
@@ -0,0 +1,8 @@
+﻿namespace DesignPatterns._01_Creational._01_Abstract_Factory;
+
+// Abstract Factory
+public abstract class AutoSocorroFactory
+{
+    public abstract Guincho CriarGuincho();
+    public abstract Veiculo CriarVeiculo(string modelo, Porte porte);
+}
---
+++ Formação/Arquiteto-de-Software/src/Fundamentos/DesignPatterns/01-Creational/01-Abstract-Factory/SocorroVeiculoMedioFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns._01_Creational._01_Abstract_Factory;
+
+// Concrete Factory
+public class SocorroVeiculoMedioFactory : AutoSocorroFactory
+{
+    public override Guincho CriarGuincho()
+    {
+        return GuinchoCreator.Criar(Porte.Medio);
+    }
+
+    public override Veiculo CriarVeiculo(string modelo, Porte porte)
+    {
+        return VeiculoCreator.Criar(modelo, porte);
+    }
+}
