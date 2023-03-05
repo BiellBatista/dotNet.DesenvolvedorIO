e473afd6ff4f845e8019333ebe1bbde724d3e0f9
Gabriel Batista <gabriel.texteis@gmail.com>
2023-03-05 19:13:54 -0300

    conclusão do módulo 09 da aula 08 da seção "Fundamentos de Arquitetura de Software"

---
+++ Formação/Arquiteto-de-Software/src/Fundamentos/DesignPatterns/02-Structural/03-Composite/DomainMessage.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns._02_Structural._03_Composite;
+
+public class DomainMessage : IMessage
+{
+    public string Nome { get; set; }
+
+    public DomainMessage(string name)
+    {
+        Nome = name;
+    }
+
+    public void ExibirMensagens(int sub)
+    {
+        Console.WriteLine(new string('-', sub) + Nome);
+    }
+}
---
+++ Formação/Arquiteto-de-Software/src/Fundamentos/DesignPatterns/02-Structural/03-Composite/IMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace DesignPatterns._02_Structural._03_Composite;
+
+public interface IMessage
+{
+    string Nome { get; set; }
+
+    void ExibirMensagens(int sub);
+}
